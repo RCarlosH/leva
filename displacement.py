223a0ec346efa852f2826c137ca920f720d15253
RCarlosH <up210281@alumnos.upa.edu.mx>
2023-11-28 20:44:46 -0600

    Added cycloidal and harmonic

--- displacement.py
+++ displacement.py
@@ -7,9 +7,9 @@ import math
 # time_elapsed:     T_i     T_j
 
 def displacement(typeof, height_if, height_delta, time_interval, time_elapsed):
-    # Cam Follower Kinematics for Constant Velocity Motion: Rise
     if typeof == 'Constant':
         if height_delta > 0:
+            # Cam Follower Kinematics for Constant Velocity Motion: Rise
             pos = height_if + ((height_delta * time_interval) / time_elapsed)
         else:
             # Cam Follower Kinematics for Constant Velocity Motion: Fall
@@ -17,19 +17,23 @@ def displacement(typeof, height_if, height_delta, time_interval, time_elapsed):
 
     elif typeof == 'Cycloidal':
         # Cam Follower Kinematics for Cycloidal Motion: Rise
-        pos = (height_if +
-                height_delta * ((time_interval / time_elapsed) - (1/math.pi *
-                math.sin * (2 * math.pi * time_interval / time_elapsed))))
-
-        # Cam Follower Kinematics for Cycloidal Motion: Fall
-        pass
+        if height_delta > 0:
+            pos = (height_if +
+                    height_delta * ((time_interval / time_elapsed) - (1/math.pi *
+                    math.sin * (2 * math.pi * time_interval / time_elapsed))))
+        else:
+            # Cam Follower Kinematics for Cycloidal Motion: Fall
+            pos = (height_if - height_delta * (1-(time_interval/time_elapsed) +
+                1/(2*math.pi()) * math.sin(2*math.pi*time_interval/time_elapsed)))
 
     elif typeof == 'Harmonic':
-        # Cam Follower Kinematics for Harmonic Motion: Rise
-        pass
+        if height_delta > 0:
+            # Cam Follower Kinematics for Harmonic Motion: Rise
+            pos = height_if + (height_delta/2)*(1-math.cos(math.pi()*time_interval/time_elapsed))
 
         # Cam Follower Kinematics for Harmonic Motion: Fall
-        pass
+        else:
+            pos = height_if - (height_delta/2)*(1+math.cos(math.pi()*time_interval/time_elapsed))
 
     else:
         raise ValueError
